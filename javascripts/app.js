97bf2e3c194113140473d2f7841c12fab04a15da
Sofía <sofianombela@gmail.com>
2018-12-05 19:03:48 +0100

    Bonus 2 - Part 2.11

--- javascripts/app.js
+++ javascripts/app.js
@@ -50,22 +50,30 @@ function moveForward(rover){
  // console.log("The start position is [" + rover.x + ", " + rover.y + "]");
   switch (rover.direction) {
     case "N":
-    if (rover.y != 0) {
+    if (board[rover.y - 1][rover.x] == "O"){
+      console.log("The rover has crushed with a obstacle");
+    } else if (rover.y != 0) {
       rover.y -= 1;
     }
     break;
     case "E":
-    if (rover.x != 9) {
+    if (board[rover.y][rover.x + 1] == "O"){
+      console.log("The rover has crushed with a obstacle");
+    } else if (rover.x != 9) {
       rover.x += 1;
     }
     break;
     case "S":
-    if (rover.y != 9) {
+    if (board[rover.y + 1][rover.x] == "O"){
+      console.log("The rover has crushed with a obstacle");
+    } else if (rover.y != 9) {
       rover.y += 1;
     }
     break;
     case "W":
-    if (rover.x != 0) {
+    if (board[rover.y ][rover.x - 1] == "O"){
+      console.log("The rover has crushed with a obstacle");
+    } else if (rover.x != 0) {
       rover.x -= 1;
     }
     break;
@@ -78,22 +86,30 @@ function moveBackward(rover){
   // console.log("The start position is [" + rover.x + ", " + rover.y + "]");
    switch (rover.direction) {
      case "N":
-     if (rover.y != 9) {
+     if (board[rover.y + 1][rover.x] == "O"){
+      console.log("The rover has crushed with a obstacle");
+      } else if (rover.y != 9) {
        rover.y += 1;
      }
      break;
      case "E":
-     if (rover.x != 0) {
+     if (board[rover.y][rover.x -1] == "O"){
+      console.log("The rover has crushed with a obstacle");
+      } else if (rover.x != 0) {
        rover.x -= 1;
      }
      break;
      case "S":
-     if (rover.y != 0) {
+     if (board[rover.y - 1][rover.x] == "O"){
+      console.log("The rover has crushed with a obstacle");
+     } else if (rover.y != 0) {
        rover.y -= 1;
      }
      break;
      case "W":
-     if (rover.x != 9) {
+     if (board[rover.y][rover.x +1] == "O"){
+      console.log("The rover has crushed with a obstacle");
+     } else if (rover.x != 9) {
        rover.x += 1;
      }
      break;
@@ -127,8 +143,63 @@ function executeCommand(command){
     }
   }
 
-  console.log(rover.travelLog);
+  // console.log(rover.travelLog);
+  printGrid(rover);
+  
 }
 
+var board = [
+  [" ","O"," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," ","O"," "," "," "],
+  [" "," ","O"," "," "," "," "," "," "," "],
+  [" "," "," "," "," "," "," "," "," "," "],
+];
+
+/*function obstacles(rover) {
+
+  for (var i = 0; i < board.length; i++){
+    var row = board[i];
+
+    for (var j = 0; j < board.length; j++){
+      var column = row[j];
+      if (column === "O"){
+      console.log("There is an obstacle in " + i + ", " + j);
+      }
+    }
+  }
+}*/
 
 
+
+function printGrid(rover) {
+  var text = ""
+  for (var i = 0; i < 10; i++) {
+    text += "\n-----------------------------------------";
+    text += "\n|";
+    for (var j = 0; j < 10; j++) {
+      if (rover.y === i && rover.x === j) {
+        if (rover.direction == "N") {
+          text += " ↑ |";
+        } else if (rover.direction == "W") {
+         text += " ← |" 
+        } else if (rover.direction == "E") {
+          text += " → |" 
+        } else {
+          text += " ↓ |" 
+        }
+      } else if (board[i][j] === "O") {
+        text += " O |";
+      } else {
+        text += "   |";
+      }
+    }
+  }
+  text += "\n-----------------------------------------";
+  console.log(text);
+}
